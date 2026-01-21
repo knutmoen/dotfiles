ee8e7b9acb618fdaf173d2c537e6f939dc0ffd05
Knut Moen Sælø <knutmsalo@gmail.com>
2026-01-21 10:09:45 +0100

    Add function for java switch and maven build

--- stow/zsh/.zsh/aliases/dev.zsh
+++ stow/zsh/.zsh/aliases/dev.zsh
@@ -24,10 +24,15 @@ alias mvnfmt='mvn spotless:apply'
 alias mvnsite='mvn site'
 alias mvnverify='mvn verify'
 alias mvnsh='mvn dependency:tree'
+
 alias tmuxstatus='[ -n "$TMUX" ] && echo "i tmux" || echo "ikke i tmux"'
 alias restkall="$HOME/dotfiles/scripts/rest-kall.sh"
 
 alias loc="npx sloc --format cli-table --format-option head --exclude 'build|\\.svg$\\.xml' ./"
+
+jm() {
+  j "$1" && mvnist
+}
 
 mvn-auto-java() {
   local root="${1:-.}"
